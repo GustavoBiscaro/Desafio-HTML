2386454e7469d209f7c9de3d15e001d7931f514a
Gustavo Miguel <gustavo.biscaro@gmail.com>
2023-10-04 15:33:47 -0300

    corrigir

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let nome = document.querySelector('#nome');
+let dataNasc = document.querySelector('#nascimento');
+let genero = document.querySelector('.genero').value;
+
+function exibir(){
+    console.log(genero)
+}
