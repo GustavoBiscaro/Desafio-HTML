7be40f05f5d857a0a2c43e5732f659f4b769869d
Gustavo Miguel <gustavo.biscaro@gmail.com>
2023-10-05 12:12:36 -0300

    country

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,14 @@ const dataNasc = document.querySelector("#nascimento");
 const email = document.querySelector("#email");
 const senha = document.querySelector('#password');
 const mesmaSenha = document.querySelector('#repeat-password');
+const pais = document.querySelector('#pais');
+const botao = document.querySelector('#exibir')
 const media = document.querySelector('#nota');
+const avaliacao = document.querySelector('#avaliar');
+
+console.log(botao)
+botao.addEventListener('click', () => {
+    console.log(pais.value)
+})
+
+
