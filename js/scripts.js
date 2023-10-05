85bc1ada5bafa886e82e744cbfe9f5c0e279d11a
Gustavo Miguel <gustavo.biscaro@gmail.com>
2023-10-05 13:10:55 -0300

    some adjusts

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
+const envioRegistro = document.querySelector('#envio')
+
+/*Campos comuns*/
 const nome = document.querySelector("#nome");
 const dataNasc = document.querySelector("#nascimento");
 const email = document.querySelector("#email");
 const senha = document.querySelector('#password');
 const mesmaSenha = document.querySelector('#repeat-password');
-const pais = document.querySelector('#pais');
-const botao = document.querySelector('#exibir')
+
 const media = document.querySelector('#nota');
 const avaliacao = document.querySelector('#avaliar');
 
-console.log(botao)
-botao.addEventListener('click', () => {
-    console.log(pais.value)
+/* Selects */
+const pais = document.querySelector('#pais');
+const estado = document.querySelector('#estado');
+const cidade = document.querySelector('#cidades');
+
+/* radios */
+
+envioRegistro.addEventListener('click', ()=> {
+    console.log(cidade.value)
 })
 
 
